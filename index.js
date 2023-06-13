6e2103272fe874a7f73e96fdff69d2371ef7c34f
Duc Pham <ducpham@Ducs-MacBook-Pro.local>
2023-06-13 18:20:54 +0700

    dd

--- index.js
+++ index.js
@@ -1,80 +1,89 @@
+const fs = require('fs-extra');
+const axios = require('axios');
 const { BlobServiceClient } = require('@azure/storage-blob');
-const { v1: uuidv1 } = require('uuid');
-require('dotenv').config();
-var Parser = require('csv-parse');
-const fs = require('fs');
-const filename = './mappings.csv';
-const jszip = require('jszip');
 const AdmZip = require('adm-zip');
+const CSVToJSON = require('csvtojson');
+const filename = './mappings.csv';
 
-const initConnection = () => {
-  const BLOB_STORAGE_CONNECTION_STRING =
-   '';
-  const BLOB_STORAGE_CONTAINER_NAME = 'ca-media';
-  const blobClientService = BlobServiceClient.fromConnectionString(
-    BLOB_STORAGE_CONNECTION_STRING
-  );
-  const containerClient = blobClientService.getContainerClient(
-    BLOB_STORAGE_CONTAINER_NAME
-  );
-  return { blobClientService, containerClient };
-};
+const connectionString =
+  '';
+const containerName = 'ca-media';
 
-const getDataFromCSV = () => {
-  const data = [];
-  fs.createReadStream(filename)
-    .pipe(Parser.parse({ delimiter: ',' }))
-    .on('data', (r) => {
-      // console.log(r);
-      data.push(r);
-    })
-    .on('end', () => {
-      console.log(data);
-    });
-  return data;
-};
+async function downloadAndUploadImage(url, imgId, color, shadow) {
+  const tempFolderPath = './temp';
 
-const unzipDirectory = async (source, outputDirectory) => {
-  const zip = new AdmZip(source);
-  return new Promise((resolve, reject) => {
-    zip.extractAllToAsync(outputDirectory, true, (error) => {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log(`Extracted to "${outputDirectory}" successfully`);
-        resolve();
-      }
-    });
-  });
-};
+  const blobServiceClient =
+    BlobServiceClient.fromConnectionString(connectionString);
+  const containerClient = blobServiceClient.getContainerClient(containerName);
 
-async function main() {
-  try {
-    console.log('Azure Blob storage v12 - JavaScript quickstart sample');
-    const { blobClientService, containerClient } = initConnection();
-    //Read CSV file:
-    // const data = getDataFromCSV();
-    const outputDirectory = './ca-media/143000/';
-    // await unzipDirectory('./ca-media/143000_ffffff_1.zip', outputDirectory);
-    const data = Buffer.from(outputDirectory, "base64");
-    const blockBlobClient = containerClient.getBlockBlobClient("images");
-    console.log(blockBlobClient)
-    const response = await blobClientService.uploadData(data, {
-      blobHTTPHeaders: {
-        blobContentType: "application/directory",
-      },
-    });
-    if (response._response.status !== 201) {
-      throw new Error(
-        `Error uploading document ${blockBlobClient.name} to container ${blockBlobClient.containerName}`
-      );
+  const folderPath = `images/${imgId}/${color}/${shadow}`;
+
+  const tempFilePath = `${tempFolderPath}/${imgId}.zip`;
+
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+  await fs.ensureDir(tempFolderPath);
+  console.log(tempFolderPath);
+  await fs.writeFile(tempFilePath, response.data);
+
+  const zip = new AdmZip(tempFilePath);
+  const zipEntries = zip.getEntries();
+
+  for (const zipEntry of zipEntries) {
+    if (!zipEntry.isDirectory) {
+      const fileName = zipEntry.entryName;
+      const fileContent = zipEntry.getData();
+
+      const blobName = `${folderPath}/${fileName}`;
+      const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+      await blockBlobClient.upload(fileContent, fileContent.length);
+      console.log(`Uploaded ${fileName} to Azure Blob Storage in ${blobName}.`);
     }
-  } catch (err) {
-    console.log(`Error: ${err.message}`);
   }
+
+  await fs.remove(tempFilePath);
+
+  console.log('Download and upload completed.');
 }
 
-main()
-  .then(() => console.log('Done'))
-  .catch((ex) => console.log(ex.message));
+function main() {
+  CSVToJSON()
+    .fromFile(filename)
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        console.log(
+          '3333333',
+          element.IMAGE_ID,
+          element.DEF_HEX,
+          element.SHADOW
+        );
+        if (
+          element.IMAGE_ID !== '' &&
+          element.DEF_HEX !== '' &&
+          element.SHADOW !== ''
+        ) {
+          const HEX =
+            element.DEF_HEX.toString().length < 6
+              ? Array(6 - element.DEF_HEX.toString().length)
+                  .fill(0)
+                  .join('')
+              : element.DEF_HEX;
+          const imageUrl = `https://img1.imaca.de/output/downloadimg.php?imgid=${element.IMAGE_ID}&color=${element.DEF_HEX}&shadow=${element.SHADOW}&clientid=76-7cc8c95bd83160aab122990635f1b384-06122022`;
+          downloadAndUploadImage(
+            imageUrl,
+            element.IMAGE_ID,
+            element.DEF_HEX,
+            element.SHADOW
+          ).catch((error) => {
+            console.error('Error:', error);
+          });
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+main();
